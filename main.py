8858807e04ec41a7e78f17618ae7eff6bbb645d0
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-07 15:59:57 +0900

    Dynamic Programming approach to the Knapsack Problem

--- main.py
+++ main.py
@@ -28,20 +28,22 @@ def knapsack_brute_force(items, capacity):
     n = len(items)
     best_value = 0
     best_combination = None
+    used_capacity = None
+    runtime_counter = 0
 
     for r in range(n + 1):
         for combination in combinations(items, r):
+            runtime_counter += 1
             total_weight = sum(item["weight"] for item in combination)
             total_value = sum(item["value"] for item in combination)
             if total_weight <= capacity and total_value > best_value:
                 best_value = total_value
                 best_combination = combination
+                used_capacity = total_weight
+                
 
-    return best_value, best_combination
+    return best_value, best_combination, used_capacity
 
-print(knapsack_brute_force(items, capacity))
-# Result: (270, ({'weight': 10, 'value': 60}, {'weight': 20, 'value': 100}, {'weight': 5, 'value': 30}, {'weight': 15, 'value': 80}))
-# Highest value was 270
 
 def knapsack_greedy_function(items, capacity):
     items_sorted = sorted(items, key=lambda x: x["value"] / x["weight"], reverse=True)
@@ -57,5 +59,33 @@ def knapsack_greedy_function(items, capacity):
 
     return total_value, chosen_items, total_weight
 
+
+
+def knapsack_dynamic_function(items, capacity):
+    n = len(items)
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+
+    for i in range(n + 1):
+        for w in range(capacity + 1):
+            if i == 0 or w == 0:
+                dp[i][w] = 0
+            elif items[i-1]["weight"] <= w:
+                dp[i][w] = max(items[i-1]["value"] + dp[i-1][w-items[i-1]["weight"]], dp[i-1][w])
+            else:
+                dp[i][w] = dp[i-1][w]
+
+    w = capacity
+    chosen_items = []
+
+    for i in range(n, 0, -1):
+        if dp[i][w] != dp[i-1][w]:
+            chosen_items.append(items[i-1])
+            w -= items[i-1]["weight"]
+
+    total_value = dp[n][capacity]
+    return total_value, chosen_items
+
+
 print(knapsack_brute_force(items, capacity))
 print(knapsack_greedy_function(items, capacity))
+print(knapsack_dynamic_function(items, capacity))
